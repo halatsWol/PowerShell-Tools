1fa0c2668e5e7388a6d1b19a4c861c199822277f
halatsWol <130136189+halatsWol@users.noreply.github.com>
2024-08-19 14:49:57 +0200

    wrong var used

--- modules/Repair-System/Repair-System.psm1
+++ modules/Repair-System/Repair-System.psm1
@@ -329,7 +329,7 @@ function Repair-RemoteSystem {
                     start-service appidsvc
                     start-service cryptsvc
                     $successMessage = "Windows Update Cleanup successfully.`r`nSoftwareDistribution and catroot2 folders have been renamed."
-                    Write-Verbose $errorMessage
+                    Write-Verbose $successMessage
                     Add-Content -Path $using:updateCleanupLog -Value "[$using:currentDateTime] - INFO:`r`n$successMessage"
                 } catch {
                     $errorMessage = "An error occurred while performing Windows Update Cleanup: $_"
