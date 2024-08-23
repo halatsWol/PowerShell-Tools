8d9ae2c3026890292f5b7a6bd65b0e29a74b3711
halatsWol <halatschek.wolfram@gmail.com>
2024-08-24 00:01:31 +0200

    add PS import-module

--- modules/Repair-System/install.cmd
+++ modules/Repair-System/install.cmd
@@ -1,22 +1,20 @@
 @echo off
 setlocal
 
-REM Define source and destination paths
 set "sourcePath=%~dp0"
 set "destinationPath=%programfiles%\WindowsPowerShell\Modules\Repair-System"
 
-REM Create destination directory if it does not exist
 if not exist "%destinationPath%" (
     mkdir "%destinationPath%"
 )
-
-REM Copy the files
 copy "%sourcePath%Repair-System.psm1" "%destinationPath%"
 copy "%sourcePath%Repair-System.psd1" "%destinationPath%"
 
 REM Check if the copy was successful
 if %errorlevel% equ 0 (
     echo Files copied successfully.
+    REM Import the module in PowerShell
+    powershell.exe -Command "Import-Module Repair-System"
 ) else (
     echo Error occurred while copying files.
 )
