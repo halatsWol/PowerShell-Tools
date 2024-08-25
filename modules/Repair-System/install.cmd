f379a8d8d5507cd629a77abf5e7bca4398f4bc2a
halatsWol <halatschek.wolfram@gmail.com>
2024-08-25 10:19:53 +0200

    wording change

--- modules/Repair-System/install.cmd
+++ modules/Repair-System/install.cmd
@@ -15,7 +15,7 @@ if %errorlevel% equ 0 (
     echo Files copied successfully.
     REM Import the module in PowerShell
     powershell.exe -Command "Import-Module Repair-System"
-    echo Module imported. Please restart any PowerShell session.
+    echo Module installed. Please restart any PowerShell session.
 ) else (
     echo Error occurred while copying files.
 )
