f69cc7ccdd3ace28fe261f03134acfbf1e7a913f
halatsWol <halatschek.wolfram@gmail.com>
2024-10-06 22:27:27 +0200

    get-help update

--- modules/Shortcuts/Shortcuts.psm1
+++ modules/Shortcuts/Shortcuts.psm1
@@ -1,21 +1,34 @@
 function Get-Shortcut {
 	<#
-		.SYNOPSIS
-		Get information about a shortcut file.
+	.SYNOPSIS
+	Get information about a shortcut file.
 
-		.DESCRIPTION
-		prints information about a shortcut file.
+	.DESCRIPTION
+	prints information about a shortcut file.
 
-		.PARAMETER path
-		The path to the shortcut file.
+	.PARAMETER path
+	The path to the shortcut file.
 
-		.EXAMPLE
-		Get-Shortcut -path 'C:\Users\Public\Desktop\Notepad.lnk'
+	.EXAMPLE
+	Get-Shortcut -path 'C:\Users\Public\Desktop\Notepad.lnk'
 
-		.NOTES
-		Author: Wolfram Halatschek
-		E-Mail: halatschek.wolfram@gmail.com
-		Date: 2024-10-04
+	.NOTES
+    This script is provided as-is and is not supported by Microsoft. Use it at your own risk.
+    WinRM must be enabled and configured on the remote computer for this script to work. Using IP addresses may require additional configuration.
+    Using this script may require administrative privileges on the remote computer.
+    In a Domain, powershell can be executed locally as the user wich has the necessary permissions on the remote computer.
+
+	WARNING:
+    NEVER CHANGE SYSTEM SETTINGS OR DELETE FILES WITHOUT PERMISSION OR AUTHORIZATION.
+    NEVER CHANGE SYSTEM SETTINGS OR DELETE FILES WITHOUT UNDERSTANDING THE CONSEQUENCES.
+    NEVER RUN SCRIPTS FROM UNTRUSTED SOURCES WITHOUT REVIEWING AND UNDERSTANDING THE CODE.
+    DO NOT USE THIS SCRIPT ON PRODUCTION SYSTEMS WITHOUT PROPER TESTING. IT MAY CAUSE DATA LOSS OR SYSTEM INSTABILITY.
+
+
+    Author: Wolfram Halatschek
+    E-Mail: halatschek.wolfram@gmail.com
+    Date: 2024-10-06
+
 	#>
 	[CmdletBinding()]
 	param(
@@ -65,30 +78,53 @@ function Get-Shortcut {
 
 function Set-Shortcut {
 	<#
-		.SYNOPSIS
-		Create or modify a shortcut file.
+	.SYNOPSIS
+	Create or modify a shortcut file.
 
-		.DESCRIPTION
-		Create or modify a shortcut file.
+	.DESCRIPTION
+	Create or modify a shortcut file.
 
-		.PARAMETER LinkPath
-		Destination Path of the Shortcut
+	.PARAMETER LinkPath
+	Destination Path of the Shortcut
 
-		.PARAMETER Hotkey
-		Hotkey of the Shortcut
+	.PARAMETER Hotkey
+	Hotkey of the Shortcut
 
-		.PARAMETER IconLocation
-		Icon Location of the Shortcut, if not set, the target icon is used (value ',1')
+	.PARAMETER IconLocation
+	Icon Location of the Shortcut, if not set, the target icon is used (value ',1')
 
-		.PARAMETER Arguments
-		Arguments of the Shortcut
+	.PARAMETER Arguments
+	Arguments of the Shortcut
 
-		.PARAMETER TargetPath
-		Target Path of the Shortcut
+	.PARAMETER TargetPath
+	Target Path of the Shortcut
 
-		.PARAMETER WorkingDirectory
-		Working-Directory / 'Start In'-Directory of the Shortcut, If not set, the target directory is used
-		If WorkingDirectory needs to be empty, please set it to an empty string
+	.PARAMETER WorkingDirectory
+	Working-Directory / 'Start In'-Directory of the Shortcut, If not set, the target directory is used
+	If WorkingDirectory needs to be empty, please set it to an empty string
+
+
+	.EXAMPLE
+	Set-Shortcut -LinkPath 'C:\Users\Public\Desktop\Notepad.lnk' -Hotkey 'Ctrl+Alt+N' -IconLocation 'C:\Windows\System32\notepad.exe,0' -Arguments 'C:\Windows\System32\notepad.exe' -TargetPath 'C:\Windows\System32\notepad.exe' -WorkingDirectory 'C:\Windows\System32'
+
+
+	.NOTES
+    This script is provided as-is and is not supported by Microsoft. Use it at your own risk.
+    WinRM must be enabled and configured on the remote computer for this script to work. Using IP addresses may require additional configuration.
+    Using this script may require administrative privileges on the remote computer.
+    In a Domain, powershell can be executed locally as the user wich has the necessary permissions on the remote computer.
+
+	WARNING:
+    NEVER CHANGE SYSTEM SETTINGS OR DELETE FILES WITHOUT PERMISSION OR AUTHORIZATION.
+    NEVER CHANGE SYSTEM SETTINGS OR DELETE FILES WITHOUT UNDERSTANDING THE CONSEQUENCES.
+    NEVER RUN SCRIPTS FROM UNTRUSTED SOURCES WITHOUT REVIEWING AND UNDERSTANDING THE CODE.
+    DO NOT USE THIS SCRIPT ON PRODUCTION SYSTEMS WITHOUT PROPER TESTING. IT MAY CAUSE DATA LOSS OR SYSTEM INSTABILITY.
+
+
+    Author: Wolfram Halatschek
+    E-Mail: halatschek.wolfram@gmail.com
+    Date: 2024-10-06
+
 	#>
 	[CmdletBinding()]
 	param(
