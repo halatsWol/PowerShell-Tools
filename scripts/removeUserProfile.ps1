43b681ce50377c63c01182ac1e2d7192e95db63e
halatsWol <130136189+halatsWol@users.noreply.github.com>
2024-10-28 10:02:06 +0100

    remove Recurse

--- scripts/removeUserProfile.ps1
+++ scripts/removeUserProfile.ps1
@@ -69,8 +69,6 @@ else{
             $regUserPathKey3=Get-Item -Path ("$regUserPath\$profileListId" +"_Classes")
             Log-Message "Backing up Registry Profile List of $UserName to $outputFilePath"
             Start-Process -FilePath "reg.exe" -ArgumentList "export `"$profileListKey`" `"$outputFilePath`" /y" -NoNewWindow -Wait
-            Log-Message "Deleting $regProfileListPath\$profileListId"
-            Remove-Item -Path $regProfileListPath\$profileListId -Force
 
             # get network drives
             $drives = Get-ChildItem -Path "$regUserPath\$profileListId\Network"
@@ -94,18 +92,19 @@ else{
                     Add-Content -Path $printerListFile -Value $printer
                 }
             }
-
-
+            
+            Log-Message "Deleting $regProfileListPath\$profileListId"
+            Remove-Item -Path $regProfileListPath\$profileListId -Force -Recurse
             $outputFilePath2 = "$remoteTempPath\HKey_UsersBackup_$UserName"+"_$currentDateTime.reg"
             $outputFilePath3 = "$remoteTempPath\HKey_Users_Classes_Backup_$UserName"+"_$currentDateTime.reg"
             Log-Message "Backing up User Profile Registry to $outputFilePath2"
             Start-Process -FilePath "reg.exe" -ArgumentList "export `"$regUserPathKey2`" `"$outputFilePath2`" /y" -NoNewWindow -Wait
             Log-Message "Deleting $regUserPath\$profileListId"
-            Remove-Item -Path $regUserPath\$profileListId -Force
+            Remove-Item -Path $regUserPath\$profileListId -Force -Recurse
             Log-Message "Backing up User Profile Registry Classes to $outputFilePath3"
             Start-Process -FilePath "reg.exe" -ArgumentList "export `"$regUserPathKey3`" `"$outputFilePath3`" /y" -NoNewWindow -Wait
             Log-Message $("Deleting $regUserPath\$profileListId" +"_Classes")
-            Remove-Item -Path ("$regUserPath\$profileListId" +"_Classes") -Force
+            Remove-Item -Path ("$regUserPath\$profileListId" +"_Classes") -Force -Recurse
             Log-Message "Registry Profile List and User Profile Backup completed"
             Log-Message "Renaming Profile Folder $profilePath"
             Rename-Item -Force -Path $profilePath -NewName $profilePathOld
