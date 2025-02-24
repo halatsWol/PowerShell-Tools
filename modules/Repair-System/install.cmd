a2d3eee98385acbc288a75830f09adcdd89753ef
halatsWol <halatschek.wolfram@gmail.com>
2025-02-24 06:14:54 +0100

    renamed module

--- modules/Repair-System/install.cmd
+++ modules/Repair-System/install.cmd
@@ -11,15 +11,15 @@ if not exist "%destinationPath%" (
 if not exist "%destinationPathHelp%" (
     mkdir "%destinationPathHelp%"
 )
-copy "%sourcePath%Repair-System.psm1" "%destinationPath%"
-copy "%sourcePath%Repair-System.psd1" "%destinationPath%"
-copy "%sourcePath%en-US\about_Repair-System.help.txt" "%destinationPathHelp%"
+copy "%sourcePath%RepairSystem.psm1" "%destinationPath%"
+copy "%sourcePath%RepairSystem.psd1" "%destinationPath%"
+copy "%sourcePath%en-US\about_RepairSystem.help.txt" "%destinationPathHelp%"
 
 REM Check if the copy was successful
 if %errorlevel% equ 0 (
     echo Files copied successfully.
     REM Import the module in PowerShell
-    powershell.exe -Command "Import-Module Repair-System"
+    powershell.exe -Command "Import-Module RepairSystem"
     echo Module installed. Please restart any PowerShell session.
 ) else (
     echo Error occurred while copying files.
