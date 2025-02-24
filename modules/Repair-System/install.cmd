661ed576aace6966428eea91f5754207f2cba3b9
halatsWol <halatschek.wolfram@gmail.com>
2025-02-24 06:36:44 +0100

    fixed paths

--- modules/Repair-System/install.cmd
+++ modules/Repair-System/install.cmd
@@ -1,9 +1,10 @@
 @echo off
 setlocal
 
-set "sourcePath=%~dp0"
-set "destinationPath=%programfiles%\WindowsPowerShell\Modules\Repair-System"
-set "destinationPathHelp=%destinationPath%\en-US"
+set moduleName=RepairSystem
+set sourcePath=%~dp0
+set destinationPath=%programfiles%\WindowsPowerShell\Modules\%moduleName%
+set destinationPathHelp=%destinationPath%\en-US
 
 if not exist "%destinationPath%" (
     mkdir "%destinationPath%"
@@ -11,15 +12,15 @@ if not exist "%destinationPath%" (
 if not exist "%destinationPathHelp%" (
     mkdir "%destinationPathHelp%"
 )
-copy "%sourcePath%RepairSystem.psm1" "%destinationPath%"
-copy "%sourcePath%RepairSystem.psd1" "%destinationPath%"
-copy "%sourcePath%en-US\about_RepairSystem.help.txt" "%destinationPathHelp%"
+copy "%sourcePath%\%moduleName%.psm1" "%destinationPath%"
+copy "%sourcePath%\%moduleName%.psd1" "%destinationPath%"
+copy "%sourcePath%en-US\about_%moduleName%.help.txt" "%destinationPathHelp%"
 
 REM Check if the copy was successful
 if %errorlevel% equ 0 (
     echo Files copied successfully.
     REM Import the module in PowerShell
-    powershell.exe -Command "Import-Module RepairSystem"
+    powershell.exe -Command "Import-Module %moduleName%"
     echo Module installed. Please restart any PowerShell session.
 ) else (
     echo Error occurred while copying files.
