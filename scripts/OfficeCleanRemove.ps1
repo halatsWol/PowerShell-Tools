0398616823fc6278431bfadc4ccb74f7476c9173
halatsWol <halatschek.wolfram@gmail.com>
2025-06-07 22:46:24 +0200

    init

---
+++ scripts/OfficeCleanRemove.ps1
@@ -0,0 +1,26 @@
+#####################################################################################
+# Script Name:  OfficeCleanRemove.ps1
+# Description:  This script is used to cleanly uninstall Microsoft Office 365, 2019
+#               and older versions from a system.
+#
+# Author:       Halatschek Wolfram
+# Date:         2025-06-07
+# Version:      0.1
+# Notes:        This script requires administrative privileges to run.
+#
+# Usage:        Run this script in an elevated PowerShell session.
+#               PS> cd <path to script>
+#               PS> .\OfficeCleanRemove.ps1
+#
+#               Please restart your computer after running this Script and run it
+#               again to ensure all MS Office residuals are removed.
+#
+#
+# Warning:      This script is provided "as is" without any warranty of any kind.
+#
+#       !!      The Author of this script is not responsible for any data loss or
+#               system damage caused by the use of this script. Use at your own risk.
+#
+#               If any Errors occur you wish to report to the Author, please open an
+#               issue on https://github.com/halatsWol/PowerShell-Tools
+#####################################################################################
